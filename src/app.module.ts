bcee4aab4aa48b0552a3044a818c10df65f8dcaf
U2D-8Bits <u2d.8bits@gmail.com>
2024-11-30 14:42:16 -0500

    Agregar configuración de TypeORM y módulo de configuración en app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
+/* eslint-disable prettier/prettier */
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-  imports: [],
+  imports: [
+
+    ConfigModule.forRoot({}),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PGHOST,
+      port: parseInt(process.env.PGPORT),
+      username: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    })
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
