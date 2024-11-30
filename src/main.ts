b8504e075eb2bdde682da71d3febdef49e18da9b
U2D-8Bits <u2d.8bits@gmail.com>
2024-11-30 14:23:23 -0500

    Habilitar CORS y configurar el puerto del servidor en main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+/* eslint-disable prettier/prettier */
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Enable CORS
+  app.enableCors({
+    origin: '*',
+    methods: 'GET, POST, PUT, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
+
+  const PORT = process.env.PORT ?? 3000;
+  console.log(`Server running on port ${PORT}`);
+  await app.listen(PORT);
 }
 bootstrap();
